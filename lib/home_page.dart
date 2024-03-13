b6fda95b40f17076fc3d26a4efc715cb2ef30918
Jepmcpgl <157013494+Jepmcpgl@users.noreply.github.com>
2024-03-13 08:01:20 +0800

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,7 +3,7 @@ import 'package:project_app/api.dart';
 import 'package:project_app/response_model.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -12,22 +12,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool inProgress = false;
   ResponseModel? responseModel;
-  String noDataText = "Welcome to our Dictionary!";
+  String noDataText = "Welcome, to our Dictionary!";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.grey[100],
-          child: Padding(
+    return Container(
+      color: Colors.grey[100],
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 _buildHeaderWidget(),
                 const SizedBox(height: 12),
-                _buildContent(),
+                if (inProgress)
+                  const LinearProgressIndicator()
+                else if (responseModel != null)
+                  Expanded(child: _buildResponseWidget())
+                else
+                  _noDataWidget(),
               ],
             ),
           ),
@@ -37,24 +42,61 @@ class _HomePageState extends State<HomePage> {
   }
 
   _buildHeaderWidget() {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.book,
-              color: Colors.black87,
-              size: 30,
-            ),
-            SizedBox(width: 8),
-            Text(
-              "Dictionary",
-              style: TextStyle(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(bottom: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.book,
                 color: Colors.black87,
+                size: 30,
+              ),
+              SizedBox(width: 8),
+              Text(
+                "Dictionary",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+        _buildSearchWidget(),
+        const SizedBox(height: 12),
+      ],
+    );
+  }
+
+  _buildResponseWidget() {
+    return Container(
+      width: double.infinity,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 16),
+            Text(
+              responseModel!.word!,
+              style: TextStyle(
+                color: Colors.purple.shade600,
                 fontWeight: FontWeight.bold,
-                fontSize: 30,
+                fontSize: 22,
+              ),
+            ),
+            Text(responseModel!.phonetic ?? ""),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return _buildMeaningWidget(responseModel!.meanings![index]);
+                },
+                itemCount: responseModel!.meanings!.length,
               ),
             ),
           ],
@@ -63,54 +105,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _buildContent() {
-    return inProgress
-        ? const Center(child: CircularProgressIndicator())
-        : responseModel != null
-        ? _buildResponseWidget()
-        : Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildSearchWidget(),
-        const SizedBox(height: 12),
-        _noDataWidget(),
-      ],
-    );
-  }
-
-  _buildResponseWidget() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 16),
-        Text(
-          responseModel!.word!,
-          style: TextStyle(
-            color: Colors.purple.shade600,
-            fontWeight: FontWeight.bold,
-            fontSize: MediaQuery.of(context).size.width * 0.06,
-          ),
-        ),
-        Text(responseModel!.phonetic ?? ""),
-        const SizedBox(height: 16),
-        Expanded(
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              return _buildMeaningWidget(responseModel!.meanings![index]);
-            },
-            itemCount: responseModel!.meanings!.length,
-          ),
-        ),
-      ],
-    );
-  }
-
   _buildMeaningWidget(Meanings meanings) {
     String definitionList = "";
-    meanings.definitions?.forEach((element) {
-      int index = meanings.definitions!.indexOf(element);
-      definitionList += "\n${index + 1}. ${element.definition}\n";
-    });
+    meanings.definitions?.forEach(
+          (element) {
+        int index = meanings.definitions!.indexOf(element);
+        definitionList += "\n${index + 1}. ${element.definition}\n";
+      },
+    );
 
     return Card(
       color: Colors.grey[200],
@@ -124,7 +126,7 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(
                 color: Colors.orange.shade600,
                 fontWeight: FontWeight.bold,
-                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontSize: 20,
               ),
             ),
             const SizedBox(height: 12),
@@ -181,9 +183,13 @@ class _HomePageState extends State<HomePage> {
   }
 
   _buildSearchWidget() {
-    return Center(
-      child: SearchBar(
-        hintText: "Type here the word you want to know about",
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Type here the word you want to know about",
+          border: OutlineInputBorder(),
+        ),
         onSubmitted: (value) {
           _getMeaningFromApi(value);
         },
@@ -197,6 +203,7 @@ class _HomePageState extends State<HomePage> {
     });
     try {
       responseModel = await API.fetchMeaning(word);
+      setState(() {});
     } catch (e) {
       responseModel = null;
       noDataText = "Meaning cannot be fetched";
